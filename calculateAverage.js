5dbf100169274e5a0a8ad0a45106ec9b4d025751
MatTerrezza <72928801+MatTerrezza@users.noreply.github.com>
2026-09-16 09:53:44 +0300

    Delete calculateAverage.js

--- calculateAverage.js
+++
@@ -1,7 +0,0 @@
-function calculateAverage(data) {
-    if (!data || data.length === 0) return 0;
-    const total = data.reduce((sum, student) => sum + student.score, 0);
-    return total / data.length;
-}
-
-module.exports = calculateAverage;
