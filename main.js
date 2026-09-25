74865f82ad5d47081032a505e6e24a277c33685c
MatTerrezza <72928801+MatTerrezza@users.noreply.github.com>
2026-09-25 10:35:58 +0300

    Update main.js
    add logs

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 const calculateAverage = require('./modules/calculateAverage');
-const findTopStudent = require('./modules/findTopStudent');
-const filterFailed = require('./modules/filterFailed');
-const addLetterGrade = require('./modules/addLetterGrade');
+const findTopStudent   = require('./modules/findTopStudent');
+const filterFailed     = require('./modules/filterFailed');
+const addLetterGrade   = require('./modules/addLetterGrade');
 
 const grades = [
-    { name: "Макар", score: 85 },
-    { name: "Денис", score: 92 },
-    { name: "Анна", score: 78 },
-    { name: "Даша", score: 88 },
+    { name: "Макар",     score: 85 },
+    { name: "Денис",     score: 92 },
+    { name: "Анна",      score: 78 },
+    { name: "Даша",      score: 88 },
     { name: "Студент_X", score: 45 }
 ];
 
 const PASS_SCORE = 60;
 
-module.exports = {
-    grades,
-    PASS_SCORE,
-    calculateAverage,
-    findTopStudent,
-    filterFailed,
-    addLetterGrade
-};
+const withLetters = addLetterGrade(grades);
+
+console.log('Студенты с оценками:', withLetters);
+console.log('Средний балл:', calculateAverage(withLetters));
+console.log('Лучший студент:', findTopStudent(withLetters));
+console.log('Провалившие:', filterFailed(withLetters, PASS_SCORE));
